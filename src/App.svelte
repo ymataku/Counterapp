30126d6b30d018c2d9d709d6927cb3d91f1380d5
yama <s256282@wakayama-u.ac.jp>
2022-04-24 00:28:37 +0900

    make it better

--- src/App.svelte
+++ src/App.svelte
@@ -11,12 +11,8 @@
   main {
   text-align: center;
   padding: 1em;
-  max-width: 240px;
+  max-width: 600px;
   margin: 10px auto;
   }
-  @media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+ 
 </style>
